66a6e1f21f7cc61e0d81a0e8394f7fc9ea10de1c
ev2li <ev2li9748@163.com>
2022-07-13 23:20:51 +0800

    bufferevent_filter_zlib

--- tests/zlib_client.hpp
+++ tests/zlib_client.hpp
@@ -5,24 +5,74 @@
 #include <event2/buffer.h>
 #include <signal.h>
 #include <memory.h>
+struct ClientStatus{
+        FILE *fp = 0;
+        bool end = false;
+};
 
 bufferevent_filter_result client_filter_out(evbuffer *s, evbuffer *d,  ev_ssize_t limit, 
                 bufferevent_flush_mode mode, void *arg){
         std::cout << "client_filter_out"<< std::endl;
+        char data[1024] = {0};
+        int elen = evbuffer_remove(s, data, sizeof(data));
+        evbuffer_add(d, data, elen);
         return BEV_OK;
 }
 
 
 static void client_read_cb(bufferevent *bev, void *arg){
+        // 002接收服务端发送的ok的回复
         std::cout << "client_read_cb"<< std::endl;
         char data[1024] = {0};
         int len = bufferevent_read(bev, data, sizeof(data) -1);
+        if(strcmp(data, "OK") == 0){
+               std::cout << data << std::endl;
+               //开始发送文件,触发写入回调
+               bufferevent_trigger(bev, EV_WRITE, 0);
+
+        }else{
+                std::cout << "data" << std::endl;
+                bufferevent_free(bev);
+        }
         std::cout << data << " len:" << len <<std::endl;
         std::cout << "client_read_cb"<< std::endl;
 }
 
 static void client_write_cb(bufferevent *bev, void *arg){
+        ClientStatus *s = (ClientStatus*)arg;
+        FILE *fp = s->fp;
+        if(s->end){
+                //判断缓冲中是否有数据 (获取非过滤器buffer)过滤器绑定buffer
+               bufferevent *be = bufferevent_get_underlying(bev);
+               //获取输出缓冲及其大小
+               evbuffer *evb = bufferevent_get_output(be);
+               int len = evbuffer_get_length(evb);
+               if(len <= 0){
+                        //立刻清理(如果缓冲有数据不会发送)
+                        bufferevent_free(bev);
+                        delete s;
+                        return;
+               }
+                //刷新缓冲
+                bufferevent_flush(bev, EV_WRITE, BEV_FINISHED);
+                return;
+        }
+        if(!fp){
+                return;
+        }
         std::cout << "client_write_cb"<< std::endl;
+        //读取文件
+        char data[1024] = {0};
+        int len = fread(data, 1, sizeof(data),fp);
+
+        if(len <= 0){
+                fclose(fp);
+                s->end = true;
+                bufferevent_flush(bev, EV_WRITE, BEV_FINISHED);
+                return;
+        }
+        //发送文件
+        bufferevent_write(bev, data, len);
 }
 
 #define FILEPATH "/var/www/source/c++/libevent_example/tests/001.txt"
@@ -30,13 +80,20 @@ void client_event_cb(bufferevent *be, short events, void *arg){
         std::cout << "client_event_cb"<< events << std::endl;
         if(events & BEV_EVENT_CONNECTED){
                 std::cout << "BEV_EVENT_CONNECTED" << std::endl;
-                //2.发送文件名
+                //001.发送文件名
                 bufferevent_write(be, FILEPATH, strlen(FILEPATH));
                 //创建输出过滤
                 bufferevent* bev_filter = bufferevent_filter_new(be, 0, client_filter_out, 
-                                                        BEV_OPT_CLOSE_ON_FREE, 0 , 0 );
+                                                        BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS, 0 , 0 );
+                FILE *fp = fopen(FILEPATH, "rb");
+                if(!fp){
+                        std::cout << "open file " << FILEPATH << " failed!"<< std::endl;
+                        return;    
+                }
+                ClientStatus *s = new ClientStatus();
+                s->fp = fp;
                 //设置读取、写入和事件的回调
-                bufferevent_setcb(bev_filter, client_read_cb, client_write_cb, client_event_cb, arg);
+                bufferevent_setcb(bev_filter, client_read_cb, client_write_cb, client_event_cb, s);
                 bufferevent_enable(bev_filter, EV_READ | EV_WRITE);
         }
 
--- tests/zlib_server.hpp
+++ tests/zlib_server.hpp
@@ -5,7 +5,13 @@
 #include <event2/buffer.h>
 #include <signal.h>
 #include <memory.h>
+#include <string>
  #define SPORT 5001
+struct Status{
+        bool start = false;
+        FILE *fp = 0;
+        // std::string filename;
+};
 
 bufferevent_filter_result filter_in(evbuffer *s, evbuffer *d,  ev_ssize_t limit, 
                 bufferevent_flush_mode mode, void *arg){
@@ -18,13 +24,50 @@ bufferevent_filter_result filter_in(evbuffer *s, evbuffer *d,  ev_ssize_t limit,
 }
 
 void read_cb(bufferevent *bev, void *arg){
+        bufferevent *rbev = (bufferevent*)arg;
         std::cout << "server_read_cb"<< std::endl;
-        //2.回复OK
-        bufferevent_write(bev, "OK", 2);
+        Status *status = new Status();
+        if(!status->start){
+                //001接收文件名
+                char data[1024] = {0};
+                bufferevent_read(bev, data, sizeof(data) - 1);
+                std::string str(data);
+                // status->filename = data;
+                std::string out;
+                out +=  data;
+                //打开写入文件
+                status->fp = fopen(out.c_str() , "wb");
+                if(!status->fp){
+                        std::cout << "server open " << out <<" failed!" << std::endl;
+                        return;
+                }
+                //002.回复OK
+                bufferevent_write(rbev, "OK", 2);
+                status->start = true;
+        }
+        do {
+                 //写入文件
+                char data[1024] = {0};
+                int len = bufferevent_read(bev, data, sizeof(data));
+                if(len >= 0 ){
+                        fwrite(data, 1, len, status->fp);   
+                        fflush(status->fp);
+                }
+        } while (evbuffer_get_length(bufferevent_get_input(bev)) > 0);
+
 }
 
 void event_cb(bufferevent *bev, short events, void *arg){
-
+        std::cout << "server_event_cb"  << events << std::endl;
+        Status *status = (Status*)arg;
+        if(events & BEV_EVENT_EOF){
+                std::cout << "BEV_EVENT_EOF"<< std::endl;
+                if(status->fp){
+                        fclose(status->fp);
+                        status->fp = 0;
+                }
+                bufferevent_free(bev);
+        }
 }
 
  void listen_cb(struct evconnlistener* e,  evutil_socket_t s , struct sockaddr* a,  int socklen, void *arg){
@@ -32,16 +75,17 @@ void event_cb(bufferevent *bev, short events, void *arg){
         event_base *base = (event_base*)arg;
         //1.创建一个bufferevent用来通信
         bufferevent *bev = bufferevent_socket_new(base,  s, BEV_OPT_CLOSE_ON_FREE );
+        Status *status = new Status();
         //2.添加输入过滤，并设置输入回调函数
         bufferevent *bev_filter = bufferevent_filter_new(bev, 
                 filter_in,//输入过滤函数;
                 0, //输出过滤
                 BEV_OPT_CLOSE_ON_FREE, //关闭filter同时关闭bufferevent
                 0, //清理回调
-                0 //传递参数
+                status //传递参数
         );
         //3.设置回调(读取、事件[用来处理连接断开])
-        bufferevent_setcb(bev_filter, read_cb, 0, event_cb, 0 );
+        bufferevent_setcb(bev_filter, read_cb, 0, event_cb, bev);
         bufferevent_enable(bev_filter, EV_READ | EV_WRITE);
 }
 
